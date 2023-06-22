1598ba92d3f4043e3ecc038a8e4e629c6bd54de5
Konstantin <kostga123@mail.ru>
2023-06-22 18:06:07 +0300

    Init rep

---
+++ src/main/java/com/unigroup/dndcharlist/DndcharlistApplication.java
@@ -0,0 +1,13 @@
+package com.unigroup.dndcharlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DndcharlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DndcharlistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unigroup/dndcharlist/DndcharlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unigroup.dndcharlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DndcharlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
