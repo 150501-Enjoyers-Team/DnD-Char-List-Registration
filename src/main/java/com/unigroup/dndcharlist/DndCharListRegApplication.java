aae3e44b016e6868bacc1cda639f12266f04d132
Konstantin <kostga123@mail.ru>
2023-07-06 14:14:06 +0300

    Repository ready to work

--- src/main/java/com/unigroup/dndcharlist/DndCharListRegApplication.java
+++ src/main/java/com/unigroup/dndcharlist/DndCharListRegApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DndCharListApplication {
+public class DndCharListRegApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DndCharListApplication.class, args);
+		SpringApplication.run(DndCharListRegApplication.class, args);
 	}
 
 }
